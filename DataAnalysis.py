46848674946579251ba120eb79181415d03d8980
Sarabu97 <82670303+Sarabu97@users.noreply.github.com>
2021-05-11 12:41:21 +0200

    Update DataAnalysis.py

--- DataAnalysis.py
+++ DataAnalysis.py
@@ -1,8 +1,9 @@
 import json
-from myMQTT import *
+from MyMQTT import *
 import requests
 import sys
 import statistics
+import time
 # it is a block that has the task of obtaining statistical data on the wellness of the patient. It provides information such as:
 # - weekly report of average heart rate in time bands of the day; 
 # - weekly activity status (percentage of over threshold movement looking in pair heart rate and accelerometer measurements) (Flow chart 1);
@@ -10,19 +11,21 @@ import statistics
 # - weekly report of how long the person has been in the bedroom, using data from a motion sensor on the bedroom’s door (Flow chart 2).
 # It works as an MQTT subscriber that recieves data from ThingSpeak Adaptor and as a MQTT publisher that sends processed data to Node-RED.
 # CATALOG_URL="http://127.0.0.1:8080"
-clientID="DataAnalysisMS"
+
 # DAtopicS="iSupport/+/statistics/#" 
 # DATtopicP_base="iSupport/"
 
 class DataAnalysis():
-	def __init__(self,CATALOG_URL,topic_base):
+	def __init__(self,CATALOG_URL,topic_base,clientID):
+		self.CATALOG_URL = CATALOG_URL
 		self.DATtopicP_base = topic_base
 		self.DAtopicS = topic_base + '+/statistics/#' #/weekly and /monthly
+		self.clientID=clientID
 		self.list_dict=[]
-		self.CatalogCommunication()
 		self.WriteBaseUrl="https://api.thingspeak.com/update?api_key="
 		self.ReadBaseUrl="https://api.thingspeak.com/channels/"
-		self.client=MyMQTT(clientID,self.broker,self.port,self) #cration of MQTT subscribers
+		self.broker=''
+		self.port=0
 
 	def start(self):
 		self.client.start() 
@@ -138,42 +141,63 @@ class DataAnalysis():
 
 	
 	def CatalogCommunication(self):
-		#Retriving information about MB
-		r=requests.get(self.CATALOG_URL+f"/broker") 
-		body=r.json()
-		self.broker=body["IPaddress"]
-		self.port=body["port"]
+		#with the catalog, for retriving information
+		r=requests.get(self.CATALOG_URL+f'/broker') 
+		if self.broker and self.port:
+			print('if CatalogCommunication')
+			if not self.broker == r.json()["IPaddress"] or not self.port == r.json()["port"]: #if the broker is changed
+				self.broker = r.json()["IPaddress"]
+				self.port = r.json()["port"]
+				self.client.stop()
+				self.client=MyMQTT(self.clientID,self.broker,self.port)
+				self.start()	
+		else:
+			print('else CatalogCommunication')
+			self.broker = r.json()["IPaddress"]
+			self.port = r.json()["port"]
+			self.client=MyMQTT(self.clientID,self.broker,self.port)
+			self.start()
+
 		#Retriving information about ThingSpeak API keys
 		r=requests.get(self.CATALOG_URL+f"/patients") 
 		body2=r.json() #lista di dizionari
+		patient_ID_list=[ID["patientID"] for ID in self.list_dict]
 		for item in body2:
-			patient={"patientID":int(item["patientID"]),
-			 	"PatientInfo":{
-					  "apikey":item["apikey"],
-					  "channel":item["channel"]
-				 		},
-				 "Weekly Measurements":{ 
+			if not item["patientID"] in patient_ID_list: #if patient not in list_dict...
+				patient={"patientID":int(item["patientID"]),
+					"PatientInfo":{
+						"apikey":item["apikey"],
+						"channel":item["channel"]
+							},
+					"Weekly Measurements":{ 
+							"day":0,
+							"min_value":[1000]*3,
+							"max_value":[0]*3,
+							"mean_value":[0]*3,
+							"activity":[0]*3,
+							"bedroomstatus":[0,0]},
+					"Monthly Measurements":{
 						"day":0,
-						"min_value":[1000]*3,
-						"max_value":[0]*3,
-						"mean_value":[0]*3,
-						"activity":[0]*3,
-						"bedroomstatus":[0,0]},
-				"Monthly Measurements":{
-					"day":0,
-					"panik attack":0
-					 }
-				}	
-			self.list_dict.append(patient)
-			
+						"panik attack":0
+						}
+					}	
+				self.list_dict.append(patient) #... add patient
+			else: #if patient already in list_dict...
+				for patient in self.list_dict:
+					if patient["patientID"]==item["patientID"]:
+						patient["PatientInfo"]["apikey"]=item["apikey"] #... update apikey and channel
+						patient["PatientInfo"]["channel"]=item["channel"]			
 
 if __name__=="__main__":
 	fp = open(sys.argv[1])
 	conf = json.load(fp)
 	CATALOG_URL = conf["Catalog_url"]
 	bT = conf["baseTopic"] 
+	clientID=conf["DataAnalysis"]["clientID"]
+	fp.close()
 
-	D=DataAnalysis(CATALOG_URL,bT)
-	D.start()
+	D=DataAnalysis(CATALOG_URL,bT,clientID)
+	D.CatalogCommunication()
 	while True:
-		pass
+		D.CatalogCommunication()
+		time.sleep(120)
