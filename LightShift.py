4ea59a6bdc851f9653d531be462f49d16ee36d5c
Sarabu97 <82670303+Sarabu97@users.noreply.github.com>
2021-05-16 10:39:18 +0200

    Update LightShift.py
    Aggiunti commenti, cambiato "Ligth" in "Light" e cambiato MyMQTT in myMQTT

--- LightShift.py
+++ LightShift.py
@@ -1,33 +1,35 @@
 import json
 import requests
-from MyMQTT import *
+from myMQTT import *
 from datetime import datetime
 import sys
 
-class LigthShift():
+class LightShift():
 	def __init__(self,CATALOG_URL,bT,clientID):
 		self.dict=[] 
 		self.CATALOG_URL = CATALOG_URL
 		self.baseTopic = bT
 		self.clientID=clientID
+
+		#initialisation for broker and port
 		self.broker=''
 		self.port=0
 
 	def start(self):
 		self.client.start()
 	
-	def controlStrategy(self):
+	def controlStrategy(self): #it checks if it's time to switch on/off the lights
 		for patient in self.dict:
 			topic=f'{self.baseTopic}{patient["patientID"]}/actuators/Light'
-			times=patient["time"].split('-')
-			now=datetime.today().time()
-			t1=datetime.strptime(times[0],"%H:%M").time()
-			t2=datetime.strptime(times[1],"%H:%M").time()
+			times=patient["time"].split('-') # structure of LightsSchedule "7:00-7:30"
+			now=datetime.today().time() #current time
+			t1=datetime.strptime(times[0],"%H:%M").time() #time to switch on the lights 
+			t2=datetime.strptime(times[1],"%H:%M").time() #time to switch off the lights 
 			
-			if t1<=now<=t2:
-				if patient["status"]==1:
+			if t1<=now<=t2: # if it's time to switch on the lights... 
+				if patient["status"]==1: #... if lights are already switched on, nothing is done
 					pass
-				else:
+				else: #an actuation command is sent to to switch lights of this patient on
 					patient["status"]=1
 					msg={'patientID':patient["patientID"], 
 						'bn':'ligth_'+str(patient["patientID"]),
@@ -38,51 +40,52 @@ class LigthShift():
 						}
 					self.client.myPublish(topic,msg)
 					print(topic)
-			else:
-				if patient["status"]==1:
+			else: # if it's time to switch off the Lights... 
+				if patient["status"]==1: #... and Lights are switch on
 					patient["status"]=0
 					msg={'patientID':patient["patientID"], 
-						'bn':'ligth_'+str(patient["patientID"]),
+						'bn':'light_'+str(patient["patientID"]),
 						'e':
 							[
 								{'n':'Light','value':0, 'timestamp':time.time(),'unit':'Bool'},
 								]
 						}
-					self.client.myPublish(topic,msg) 
+					self.client.myPublish(topic,msg) #an actuation command is sent to to switch lights of this patient off
 					print(topic)
 
 	def CatalogCommunication(self):
 		#with the catalog, for retriving information
-		r=requests.get(self.CATALOG_URL+f'/broker') 
-		if self.broker and self.port:
+		r=requests.get(self.CATALOG_URL+f'/broker') #retrieve broker/port 
+		if self.broker and self.port: #if broker and port already exist...
 			print('if CatalogCommunication')
-			if not self.broker == r.json()["IPaddress"] or not self.port == r.json()["port"]: #if the broker is changed
-				self.broker = r.json()["IPaddress"]
+			if not self.broker == r.json()["IPaddress"] or not self.port == r.json()["port"]: #check if the broker is changed...
+				self.broker = r.json()["IPaddress"] #... update broker and port
 				self.port = r.json()["port"]
-				self.client.stop()
-				self.client=MyMQTT(self.clientID,self.broker,self.port)
+				self.client.stop() #stop the previous client and 
+				self.client=MyMQTT(self.clientID,self.broker,self.port) #create and start new client
 				self.start()	
-		else:
+		else: #create and start new client
 			print('else CatalogCommunication')
 			self.broker = r.json()["IPaddress"]
 			self.port = r.json()["port"]
 			self.client=MyMQTT(self.clientID,self.broker,self.port)
 			self.start()
 		
+		#retrieve patient ID and LightsSchedule
 		r=requests.get(CATALOG_URL+f'/patients') 
-		body2=r.json() #lista di dizionari
-		patient_ID_list=[ID["patientID"] for ID in self.dict]
+		body2=r.json()
+		patient_ID_list=[ID["patientID"] for ID in self.dict] #list of patient ID already retreived and present in self.dict
 		for item in body2:
-			if not item["patientID"] in patient_ID_list:
-				new_patient={"patientID":item["patientID"], "time":item["LightsSchedule"],'status':0} #status: 0 off, status: 1 on
+			if not item["patientID"] in patient_ID_list: #if the patient ID is not present in self.dict it's added
+				new_patient={"patientID":item["patientID"], "time":item["LightsSchedule"],'status':0} #light status: 0 off, status: 1 on
 				self.dict.append(new_patient)
-			else:
+			else: #if it is present "LightsSchedule" is updated
 				for patient in self.dict:
 					if patient["patientID"]==item["patientID"]:
-						patient["LightsSchedule"]=item["LightsSchedule"]
+						patient["time"]=item["LightsSchedule"]
 		
-
 if __name__=="__main__":
+	# command line argument in position 1 is the Configuration_file.json
 	fp = open(sys.argv[1])
 	conf = json.load(fp)
 	CATALOG_URL = conf["Catalog_url"]
@@ -90,8 +93,9 @@ if __name__=="__main__":
 	clientID=conf["LightShift"]["clientID"] 
 	fp.close()
 
-	LS=LigthShift(CATALOG_URL,bT,clientID)	
+	LS=LightShift(CATALOG_URL,bT,clientID)	
 	while True:
+		#every 60s retrieve broker/port and patient "LightsSchedule" and perform the control strategy to check if it's time to switch on/off lights
 		LS.CatalogCommunication()
 		LS.controlStrategy()
 		time.sleep(60)
