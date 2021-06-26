bbb17b801a828a3d30a4fe10ab866928025b8577
Sarabu97 <82670303+Sarabu97@users.noreply.github.com>
2021-06-26 11:06:25 +0200

    Delete publisher_telegram_bot.py

--- publisher_telegram_bot.py
+++
@@ -1,9 +0,0 @@
-from myMQTT import *
-test=MyMQTT("testIoTBot",'test.mosquitto.org',1883,None)
-test.start()
-for i in range(5):
-    message={"alert":"is having a Panik attack","action":"check the situation"}
-    #topic="iSupport/"+str(i)+"/telegram"
-    topic="iSupport/"+str(i)+"/telegram"
-    test.myPublish(topic,message)
-    time.sleep(3)
